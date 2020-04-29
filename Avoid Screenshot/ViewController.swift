7ee8dc263bb7c46f020d7514d8a341b5cccdd914
omrobbie <putu.roby@yahoo.com>
2020-04-30 07:05:08 +0800

    avoid screen recording

--- Avoid Screenshot/ViewController.swift
+++ Avoid Screenshot/ViewController.swift
@@ -12,5 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        avoidScreenRecording()
+    }
+
+    func avoidScreenRecording() {
+        let blankView: UIView = {
+            let v = UIView()
+            v.frame = UIScreen.main.bounds
+            v.backgroundColor = .label
+            v.isHidden = true
+            return v
+        }()
+
+        view.addSubview(blankView)
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+            UIScreen.screens.forEach {
+                blankView.isHidden = !$0.isCaptured
+            }
+        })
     }
 }
