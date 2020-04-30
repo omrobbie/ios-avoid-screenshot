e789f3f2e6668cc589046c8108b31fde2d9326eb
omrobbie <putu.roby@yahoo.com>
2020-04-30 11:07:25 +0800

    can't avoid sceenshot. iPhone shows default permission to delete image

--- Avoid Screenshot/ViewController.swift
+++ Avoid Screenshot/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        avoidScreenshot()
         avoidScreenRecording()
+    }
+
+    func avoidScreenshot() {
+        NotificationCenter.default.addObserver(forName: UIApplication.userDidTakeScreenshotNotification, object: nil, queue: nil) { (_) in
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { (_) in
+                let options = PHFetchOptions()
+                options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+                options.fetchLimit = 1
+
+                let result = PHAsset.fetchAssets(with: .image, options: options)
+
+                if result.count > 0 {
+                    PHPhotoLibrary.shared().performChanges({
+                        if let asset = result.firstObject {
+                            PHAssetChangeRequest.deleteAssets([asset] as NSArray)
+                        }
+                    }, completionHandler: nil)
+                }
+            }
+        }
     }
 
     func avoidScreenRecording() {
